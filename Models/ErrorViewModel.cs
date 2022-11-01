e9e50b11d25809d559cf773b482610105ba53f87
maddy-222 <91879300+maddy-222@users.noreply.github.com>
2022-11-01 09:24:04 +0530

    Create ErrorViewModel.cs

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace shoppingstore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
